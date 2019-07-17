0f0ad66b475b30e25b584573d8253588cd6dcebd
mitohato <mitohato14@gmail.com>
2019-07-17 13:37:50 +0900

    Add repository

---
+++ MVVM-Sample-Kotlin/app/src/main/java/com/ict/mito/mvvm_sample_kotlin/Repository.kt
@@ -0,0 +1,10 @@
+package com.ict.mito.mvvm_sample_kotlin
+
+/**
+ * Created by mitohato14 on 2019-07-17.
+ */
+class Repository {
+    fun getMessage(): String {
+        return "Hello, Android MVVM!"
+    }
+}
