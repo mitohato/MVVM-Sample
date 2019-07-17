06566148c32f61ae51c281e647d102e8bc954da6
mitohato <mitohato14@gmail.com>
2019-07-17 13:34:41 +0900

    Binding

--- MVVM-Sample-Kotlin/app/src/main/java/com/ict/mito/mvvm_sample_kotlin/MainActivity.kt
+++ MVVM-Sample-Kotlin/app/src/main/java/com/ict/mito/mvvm_sample_kotlin/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.ict.mito.mvvm_sample_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ict.mito.mvvm_sample_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+        binding?.viewmodel = MainViewModel()
     }
 }
