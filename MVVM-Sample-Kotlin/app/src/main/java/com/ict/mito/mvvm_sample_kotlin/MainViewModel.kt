27ae95b67df7c72a8f88651abc56ba0d523fcef6
mitohato <mitohato14@gmail.com>
2019-07-17 13:37:57 +0900

    Use repository

--- MVVM-Sample-Kotlin/app/src/main/java/com/ict/mito/mvvm_sample_kotlin/MainViewModel.kt
+++ MVVM-Sample-Kotlin/app/src/main/java/com/ict/mito/mvvm_sample_kotlin/MainViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.ViewModel
  * Created by mitohato14 on 2019-07-17.
  */
 class MainViewModel : ViewModel() {
+    private val repository: Repository = Repository()
+
     fun message(): String {
-        return "Hello, World!"
+        return repository.getMessage()
     }
 }
